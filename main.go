ce3018538abf3c10604b65b971e9ec0adf6c96c7
jamesfe <james.ferrara@gmail.com>
2017-05-05 09:28:30 +0200

    Counting authors per day

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
 	"bytes"
 	"compress/gzip"
 	"encoding/json"
+	"github.com/op/go-logging"
 	"os"
+	"strconv"
 	"strings"
 	"time"
-
-	"github.com/op/go-logging"
 )
 import "flag"
 
@@ -74,6 +74,25 @@ func isDonaldCertainly(comment Comment) bool {
 	return strings.ToLower(comment.Subreddit) == "the_donald"
 }
 
+func getIntTimestamp(v interface{}) int {
+	/* Sometimes the timestamps we get are float64, null, or strings. Here we check. */
+	var retVal int = 0
+	switch v.(type) {
+	case float64:
+		retVal = int(v.(float64))
+	case string:
+		parsed, err := strconv.ParseInt(v.(string), 10, 32)
+		if err != nil {
+			retVal = 0
+		} else {
+			retVal = int(parsed)
+		}
+	default:
+		retVal = 0
+	}
+	return retVal
+}
+
 func getFileReader(filename string) (*bufio.Reader, func() error) {
 	/* Opens the file (appropriately, gz or not) and returns a reader. */
 	f, err := os.Open(inFile)
@@ -81,7 +100,6 @@ func getFileReader(filename string) (*bufio.Reader, func() error) {
 		log.Debug("open")
 		log.Fatal(err)
 	}
-	// defer f.Close()
 	retVal := bufio.NewReader(f)
 
 	// If it ends in GZ it is a zip and we should ungzip it.
@@ -150,6 +168,64 @@ func UniqueAuthorsPerDayAnalysis(parameters SimpleAnalysisParameter) (UniqueAuth
 	results := New()
 	log.Warningf("%#v", results)
 
+	inFileReader, f := getFileReader(inFile)
+	defer f()
+	simpleRes := newSimpleAnalysisResult()
+
+	AuthPerDay := map[int]map[string]bool{}
+	var secondsPerDay int = 60 * 60 * 24
+
+	for {
+		var stuff, err = inFileReader.ReadBytes('\n')
+		if err != nil {
+			log.Warningf("%d, %d (initial, final) lines matched out of %d", simpleRes.TotalFirstMatches, simpleRes.TotalMatches, simpleRes.TotalLinesChecked)
+			return *results, err
+		}
+		if (parameters.CheckLines) && (simpleRes.TotalLinesChecked >= parameters.LinesToCheck) {
+			log.Errorf("Max lines of %d exceeded: %d", parameters.LinesToCheck, simpleRes.TotalLinesChecked)
+			return *results, nil
+		}
+		if isDonaldLite(stuff) {
+			simpleRes.TotalFirstMatches += 1
+			var rawJsonMap interface{}
+			newerr := json.Unmarshal(stuff, &rawJsonMap)
+
+			if newerr == nil {
+				/* we do some analysis here:
+				we calculate the day on which the post was made
+				we check if the author is in our list of authors for that day
+				if he is, we do nothing
+				otherwise we add him to the list
+				*/
+				v := rawJsonMap.(map[string]interface{})
+				author := v["author"].(string)
+				realTime := getIntTimestamp(v["created_utc"])
+				if realTime == 0 { // if it is junk, go on
+					continue
+				}
+				var extraSeconds int = int(realTime) % secondsPerDay
+				var dateInSeconds int = extraSeconds / secondsPerDay // should be the same number for every day
+
+				if AuthPerDay[dateInSeconds] == nil {
+					AuthPerDay[dateInSeconds] = make(map[string]bool)
+				}
+				if !AuthPerDay[dateInSeconds][author] {
+					AuthPerDay[dateInSeconds][author] = true
+				}
+
+				simpleRes.TotalMatches += 1
+			} else {
+				log.Errorf("JSON Parsing Error: %s", newerr)
+				return *results, newerr
+			}
+		}
+		if parameters.LogLineNotification && simpleRes.TotalLinesChecked%parameters.LineIntervalNotification == 0 {
+			log.Debugf("Read %d lines", simpleRes.TotalLinesChecked)
+		}
+		simpleRes.TotalLinesChecked++
+	}
+	// Go through the list of Authors per day and count the size of each of these things.
+
 	return *results, nil
 }
 
@@ -182,7 +258,6 @@ func main() {
 			log.Infof("%#v", simpleResults)
 		}
 	*/
-
 	var authorsPerDay, err = UniqueAuthorsPerDayAnalysis(simpleParams)
 	if err == nil {
 		log.Infof("UniqueAuthors: %#v", authorsPerDay)
