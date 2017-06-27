aa06a53b9b314be49bd10b442f88cbe805f19271
jamesfe <james.ferrara@gmail.com>
2017-06-27 09:22:43 +0200

    Refactor date generation function out

--- src/analysis/func_author_analysis.go
+++ src/analysis/func_author_analysis.go
@@ -8,6 +8,10 @@ import (
 	"github.com/jamesfe/reddit_stats/src/utils"
 )
 
+func GetDayString(input int) string {
+	return time.Unix(int64(input), 0).Format("02-01-2006")
+}
+
 func AuthorSingleLine(line []byte, result *data_types.AuthorDateTuple) bool {
 	/* Take some bytes, convert them from JSON and return the author and date as a string
 	if they are valid. */
@@ -24,7 +28,7 @@ func AuthorSingleLine(line []byte, result *data_types.AuthorDateTuple) bool {
 			result.AuthorName = v["author"].(string)
 			realTime := utils.GetIntTimestamp(v["created_utc"])
 			if realTime != 0 { // if it is junk, don't record
-				result.AuthorDate = time.Unix(int64(realTime), 0).Format("02-01-2006")
+				result.AuthorDate = GetDayString(realTime)
 				return true
 			}
 			// 	}
@@ -36,7 +40,7 @@ func AuthorSingleLine(line []byte, result *data_types.AuthorDateTuple) bool {
 }
 
 func AggregateAuthorLine(authorTuple *data_types.AuthorDateTuple, resultMap *map[string]map[string]int) {
-	/* mutate the map in place with a new result */
+	/* Increment the author's name in the date map by one for each comment he/she has made. */
 	if (*resultMap)[authorTuple.AuthorDate] != nil {
 		(*resultMap)[authorTuple.AuthorDate][authorTuple.AuthorName] += 1
 	} else {
