24e4301b1f8a6991ec6e66b2a9e8b07ca4725b77
jamesfe <james.ferrara@gmail.com>
2017-08-30 22:12:00 +0200

    Add notes about some config items

--- src/data_types/config_types.go
+++ src/data_types/config_types.go
@@ -1,11 +1,11 @@
 package data_types
 
 type FilterSettings struct {
-	SubredditListFile string `json:"list_file"`
+	SubredditListFile string `json:"list_file"` // a list of subreddits we will use to filter by
 }
 
 type InputFilterSettings struct {
-	OutputDirectory string `json:"output_dir"`
+	OutputDirectory string `json:"output_dir"` // the output directory to which our matching lines are written
 }
 
 type LongevitySettings struct {
@@ -14,6 +14,7 @@ type LongevitySettings struct {
 
 type TargetSettings struct {
 	RandomSample bool `json:"random_sample"`
+	/* true if we should sample randomly, false if we should just search the_donald */
 }
 
 type AnalysisSettings struct {
@@ -29,9 +30,9 @@ type ProfileSettings struct {
 
 type StatsConfiguration struct {
 	DataSource               string              `json:"data_source"`
-	CheckInterval            int                 `json:"check_interval"`
-	MaxLines                 int                 `json:"max_lines"`
-	CpuProfile               string              `json:"cpu_profile"`
+	CheckInterval            int                 `json:"check_interval"` // Print a note to screen every this many lines
+	MaxLines                 int                 `json:"max_lines"`      // Analyze up to this many lines
+	CpuProfile               string              `json:"cpu_profile"`    // Collect CPU profiling data
 	FilterConfiguration      FilterSettings      `json:"filter_settings",omitempty`
 	InputFilterConfiguration InputFilterSettings `json:"input_filter_settings",omitempty`
 	AnalysisConfiguration    AnalysisSettings    `json:"analysis_settings"`
