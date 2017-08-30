e1ff01efc89cee9bc752f61ac657b22dacc52f24
jamesfe <james.ferrara@gmail.com>
2017-08-30 09:18:57 +0200

    Profile work

--- src/cmd/profiles/main.go
+++ src/cmd/profiles/main.go
@@ -1,5 +1,10 @@
 package main
 
+/*
+	Here we define a command that will create a profile of users and allow us to perform some person-by-person
+	analysis of users of the various reddits.
+*/
+
 import (
 	"flag"
 	"math/rand"
@@ -16,24 +21,31 @@ var format = logging.MustStringFormatter(
 	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.8s} %{id:03x}%{color:reset} %{message}`,
 )
 
-func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
+type thing struct {
+}
 
-	configFile := flag.String("config", "", "config file (see sample in repo)")
-	flag.Parse()
-	config := utils.LoadConfigurationFromFile(*configFile)
-
-	flag.Parse()
-
-	if config.CpuProfile != "" {
-		stopIt := utils.StartCPUProfile(config.CpuProfile)
-		defer stopIt()
+func analysisFunction(config data_types.StatsConfiguration, line []byte) thing {
+	var resultItem data_types.AuthorDateSubTuple // we reuse this address for results
+	if analysis.AuthorSingleLineMultiNoFilter(line, &resultItem) {
+		if val, ok := authorProfileMap[resultItem.AuthorName]; ok {
+			// Aggregate here
+			if individualProfile, ok2 := val.CommentCountsBySub[resultItem.SubReddit]; ok2 {
+				val.CommentCountsBySub[resultItem.SubReddit] += 1
+			} else {
+				val.CommentCountsBySub[resultItem.SubReddit] = 0
+			}
+		} else {
+			authorPofileMap[resultItem.AuthorName] = data_types.AuthorProfile{CommentCountsBySub: make(map[string]int)}
+			authorProfileMap[resultItem.AuthorName].CommentCountsBySub[resultItem.SubReddit] = 1
+		}
 	}
+	return nil
+}
 
+func readFilesAndReturnAnalysis(config data_types.StatsConfiguration, analysisFunc func(config data_types.StatsConfiguration, line []byte) thing) {
 	var delim byte = '\n'
 	filesToCheck := utils.GetFilesToCheck(config.DataSource)
 	var lines int = 0
-	var resultItem data_types.AuthorDateSubTuple // we reuse this address for results
 
 	authorProfileMap := make(map[string]*data_types.AuthorProfile) // a map of usernames to profiles
 	log.Infof("Entering analysis loop.")
@@ -49,28 +61,33 @@ func main() {
 			if inputBytes, err := inFileReader.ReadBytes(delim); err != nil {
 				log.Errorf("File Error: %s", err) // maybe we are in an IO error?
 				break lineloop
-			} else if analysis.AuthorSingleLineMultiNoFilter(inputBytes, &resultItem) {
-				if config.AnalysisConfiguration.AnalysisMap["profile_users"] {
-					/* If we analyzing users, extract some features from them. */
-					if val, ok := authorProfileMap[resultItem.AuthorName]; ok {
-						// Aggregate here
-						if individualProfile, ok2 := val.CommentCountsBySub[resultItem.SubReddit]; ok2 {
-							val.CommentCountsBySub[resultItem.SubReddit] += 1
-						} else {
-							val.CommentCountsBySub[resultItem.SubReddit] = 0
-						}
-					} else {
-						authorPofileMap[resultItem.AuthorName] = data_types.AuthorProfile{CommentCountsBySub: make(map[string]int)}
-						authorProfileMap[resultItem.AuthorName].CommentCountsBySub[resultItem.SubReddit] = 1
-					}
-				}
+			} else {
+				analysisFunction(config, inputBytes)
+			}
+
+			if lines == config.MaxLines {
+				log.Infof("Max lines reached")
+				break
 			}
 		}
-		if lines == config.MaxLines {
-			log.Infof("Max lines reached")
-			break
-		}
 	}
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	configFile := flag.String("config", "", "config file (see sample in repo)")
+	flag.Parse()
+	config := utils.LoadConfigurationFromFile(*configFile)
+
+	flag.Parse()
+
+	if config.CpuProfile != "" {
+		stopIt := utils.StartCPUProfile(config.CpuProfile)
+		defer stopIt()
+	}
+
+	readFilesAndReturnAnalysis(config, analysisFunction)
 
 	if config.AnalysisConfiguration.AnalysisMap["deleted"] {
 		outputMap := analysis.AggregateByDeletedCommentCounts(far)
